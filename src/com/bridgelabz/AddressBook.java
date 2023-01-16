b3f8fb524e8b6d4e02f54c5a73fe32170b55dee1
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2023-01-16 09:35:41 +0530

    Added:update contact details from addressbook

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -33,7 +33,47 @@ public class AddressBook {
             System.out.println("exception occurred" + e);
         }
     }
-    public static void main(String[] args) {
-        read();
+    public static void edit() {
+        /**
+         * edit the contact details from address book table
+         */
+        try {
+            Connection connection = DriverManager.getConnection(jdbc, userName, password);
+            PreparedStatement prepare = connection.prepareStatement("update addressBook " + "set firstName = ?,address = ?,lastName=?,city  =?,state = ?,zip=?,PhoneNumber  =?,email=? " + "where firstName = ?");
+            System.out.print("Enter Exiting name : ");
+            String name = input.next();
+            System.out.print("Enter first name : ");
+            details.setFirstName(input.next());
+            System.out.print("Enter last name : ");
+            details.setLastName(input.next());
+            System.out.print("Enter address : ");
+            details.setAddress(input.next());
+            System.out.print("Enter city : ");
+            details.setCity(input.next());
+            System.out.print("Enter State : ");
+            details.setState(input.next());
+            System.out.print("Enter zip : ");
+            details.setZip(input.nextInt());
+            System.out.print("Enter phone number : ");
+            details.setPhoneNumber(input.nextInt());
+            System.out.print("Enter email : ");
+            details.setEmail(input.next());
+            prepare.setString(1, details.getFirstName());
+            prepare.setString(2, details.getLastName());
+            prepare.setString(3, details.getAddress());
+            prepare.setString(4, details.getCity());
+            prepare.setString(5, details.getState());
+            prepare.setInt(6, details.getZip());
+            prepare.setInt(7, details.getPhoneNumber());
+            prepare.setString(8, details.getEmail());
+            prepare.setString(9, name);
+            prepare.executeLargeUpdate();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
+    public static void main(String[] args) {
+        edit();
+    }
+
 }
